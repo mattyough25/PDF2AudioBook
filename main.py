9b3a1f7b2f739267ac816b54deb6ebddf36ca63e
mattyough25 <mgy0003@mix.wvu.edu>
2023-03-18 18:12:58 -0400

    Generated executable of initial version of PDF2AudioBook app

--- main.py
+++ main.py
@@ -1,5 +1,55 @@
 from tkinter import *
-from button_functions import get_pdf, get_audio_path, convert_pdf_to_audio
+from tkinter import filedialog
+import pyttsx3, PyPDF2
+import os
+
+###### Button Functions ######
+
+# Get PDF
+def get_pdf():
+    PDF_file = filedialog.askopenfilename(title="Open PDF File", filetypes=(("pdf files","*.pdf"), ("all files","*.*")))
+    in_file.set(PDF_file)
+
+#Get Audio Path
+def get_audio_path():
+    Audio_path = filedialog.askdirectory(title="Directory to Save Audio")
+    
+    out_path.set(Audio_path)
+
+# Convert PDF to Audio
+def convert_pdf_to_audio():
+    PDF = in_file.get()
+    pdfreader = PyPDF2.PdfReader(open(PDF,'rb'))
+    speaker = pyttsx3.init()
+    speaker.setProperty('rate', 150)
+    voices = speaker.getProperty('voices') 
+    speaker.setProperty('voice', voices[1].id) 
+
+    sRange = nPageRange.get()
+    sRange = sRange.replace('[', '')
+    sRange = sRange.replace(']', '')
+    sRange = sRange.split('-')
+    
+    nRange = list(sRange)
+    nRange[0] = int(nRange[0])
+    nRange[1] = int(nRange[1])
+
+    clean_text = ''
+    for page_num in range(nRange[0]-1, nRange[1]-1):
+        text = pdfreader.pages[page_num].extract_text()
+        clean_text += text.strip().replace('\n', ' ')
+
+    path = out_path.get()
+    file = audio_file.get()
+    file = file + '.mp3'
+
+    save_file = os.path.join(path,file)
+    speaker.save_to_file(clean_text, save_file)
+    speaker.runAndWait()
+
+    speaker.stop()
+
+###### GUI ######
 
 # Setting Main Window
 root = Tk()
@@ -11,7 +61,8 @@ sPDF_label = Label(root, text = "PDF Path")
 sPDF_label.grid(column =0, row =0)
 
 # Adding a Text Entry Box for PDF Path
-sPDF_path = Entry(root, width=50)
+in_file = StringVar()
+sPDF_path = Entry(root, width=50, textvariable=in_file)
 sPDF_path.grid(column =1, row =0)
 
 # Adding Button to Open A Dialogue Box to Select PDF
@@ -24,29 +75,33 @@ sAudioPath_label = Label(root, text = "Audio Path")
 sAudioPath_label.grid(column =0, row =1)
 
 # Adding a Text Entry Box for Audio Output Path
-sAudio_path = Entry(root, width=50)
+out_path = StringVar()
+sAudio_path = Entry(root, width=50, textvariable=out_path)
 sAudio_path.grid(column =1, row =1)
 
-# Adding Button to Open A Dialogue Box to Select PDF
+# Adding Button to Open A Dialogue Box to Select Audio Path
 Audio_btn = Button(root, bg = 'red', width = 30, 
             activebackground = 'white', text = "Get Path to Save Audio File" ,
              fg = "black", command=get_audio_path)
 Audio_btn.grid(column=2, row=1)
 
-# Adding a Label for Audio Path
+# Adding a Label for Audio File
 sAudio_label = Label(root, text = "Audio File Name")
 sAudio_label.grid(column =0, row =2)
 
 # Adding a Text Entry Box for Audio File Name
-sAudio_path = Entry(root, width=50)
-sAudio_path.grid(column =1, row =2)
+audio_file = StringVar()
+sAudio_file = Entry(root, width=50, textvariable=audio_file)
+sAudio_file.grid(column =1, row =2)
 
-# Adding a Label for Audio Path
+# Adding a Label for Page Range
 sPages_label = Label(root, text = "Page Range")
 sPages_label.grid(column =0, row =3)
 
-# Adding a Text Entry Box for Audio File Name
-nPages = Entry(root, width=50)
+# Adding a Text Entry Box for Page Range
+nPageRange = StringVar()
+nPages = Entry(root, width=50, textvariable=nPageRange)
+nPages.insert(0, "[-]")
 nPages.grid(column =1, row =3)
 
 # Adding Button to Convert PDF to Audio
